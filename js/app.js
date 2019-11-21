c8efff1f338a30481a02fffdcbb66111380dd5cd
matapaska <marta.pasikowska@gmail.com>
2019-11-21 15:19:28 +0000

    MVP done

--- js/app.js
+++ js/app.js
@@ -8,4 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
 const handleFormSubmit = function (event){
   event.preventDefault();
   console.log(event);
+  const title = event.target.title.value;
+  const author = event.target.author.value;
+  const category = event.target.category.value;
+
+  const outputBook = document.createElement('li')
+  outputBook.textContent = `${title} ${author} ${category}`;
+  outputBook.classList.add('book');
+
+  const list = document.querySelector('#reading-list');
+  list.appendChild(outputBook);
 }
